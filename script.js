0ed970b95f4f97fb4229f82419b444e6ded61c11
Souvik Hazra <117592326+Mr-R-e-X@users.noreply.github.com>
2024-04-22 09:53:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+  arr.sort((a, b) => a - b);
+	let totalCost = 0;
+	 while (arr.length > 1) {
+		 const firstRope = arr.shift();
+        const secondRope = arr.shift();
+		 const combinedLength = firstRope + secondRope;
+        arr.push(combinedLength);
+		 totalCost += combinedLength;
+		 arr.sort((a, b) => a - b);
+		  }
+    
+    return totalCost;
 }
 
 module.exports=mincost;
